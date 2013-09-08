d9bcdfbe6c275dc735bfb0173c79a5bdfa79a8a9
Sören Brinkmann <soeren.brinkmann@gmail.com>
2013-09-07 21:17:17 -0700

    Use jinja2 filters to generate URLs for static content

--- rot13/__init__.py
+++ rot13/__init__.py
@@ -4,7 +4,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static', 'rot13:static/', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('encr', '/encr')
     config.scan()
