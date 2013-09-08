b2ea08a2afef5b8ea011f243e68effca2ffac18a
Sören Brinkmann <soeren.brinkmann@gmail.com>
2013-09-08 10:12:38 -0700

    Add 'load file' button
    Add the function to load the text area from a file on disk.

--- rot13/static/rot13.js
+++ rot13/static/rot13.js
@@ -1,3 +1,22 @@
+$(document).ready(function() {
+    $('#in-lfile').change(function(e) {
+        file = $(this).prop('files')[0]
+        var reader = new FileReader()
+        reader.onload = fileLoaded
+        reader.onerror = fileError
+        reader.readAsText(file, 'UTF-8')
+    })
+})
+
+function fileLoaded(evt) {
+    var $ta = document.getElementById('inputarea')
+    $ta.value = evt.target.result
+}
+
+function fileError(evt) {
+    alert("Error reading file")
+}
+
 function encryptText()
 {
     var $ta = document.getElementById('inputarea')
@@ -9,4 +28,10 @@ function encryptText()
                 var $ta = document.getElementById('inputarea')
                 $ta.value = data},
             "text")
+}
+
+function loadFile()
+{
+    $('#in-lfile').trigger('click')
+    return false
 }
