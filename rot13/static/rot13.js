c5e6a24a26acaced3efb6e81e819d6f2d8cc1dfd
Sören Brinkmann <soeren.brinkmann@gmail.com>
2013-09-09 23:17:27 -0700

    Use relative URL to call the encryption script
    To enable running this script from URLs other than the server root, use
    relative URLs.

--- rot13/static/rot13.js
+++ rot13/static/rot13.js
@@ -23,7 +23,7 @@ function encryptText()
     var mode = $("input[name=rot_mode]:checked").val();
     var args = {rot_mode: mode,
 	        text: $ta.value}
-    $.post('/encr', args,
+    $.post('encr', args,
             function(data) {
                 var $ta = document.getElementById('inputarea')
                 $ta.value = data},
