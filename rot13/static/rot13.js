1f7d6f66aff28f4eadf04031b3cf4c7dbdeeae31
Sören Brinkmann <soeren.brinkmann@gmail.com>
2013-10-13 17:17:33 -0700

    js: Get rid of some jquery code
    Convert jquery calls to normal JS/HTML when reasonably possible.
    The only remaining jquery dependency is the UI CSS sheet which is used
    for by the <button> elements in my HTML.

--- rot13/static/rot13.js
+++ rot13/static/rot13.js
@@ -1,16 +1,15 @@
-$(document).ready(function() {
-    $('#in-lfile').change(function(e) {
-        file = $(this).prop('files')[0]
-        var reader = new FileReader()
-        reader.onload = fileLoaded
-        reader.onerror = fileError
-        reader.readAsText(file, 'UTF-8')
-    })
-})
+inf = document.getElementById('in-lfile')
+inf.onchange = function(evt) {
+    file = evt.target.files[0]
+    var reader = new FileReader()
+    reader.onload = fileLoaded
+    reader.onerror = fileError
+    reader.readAsText(file, 'UTF-8')
+}
 
 function fileLoaded(evt) {
-    var $ta = document.getElementById('inputarea')
-    $ta.value = evt.target.result
+    var ta = document.getElementById('inputarea')
+    ta.value = evt.target.result
 }
 
 function fileError(evt) {
@@ -19,33 +18,49 @@ function fileError(evt) {
 
 function encryptText()
 {
-    var $ta = document.getElementById('inputarea')
-    var mode = $("input[name=rot_mode]:checked").val();
-    var args = {rot_mode: mode,
-	        text: $ta.value}
-    $.post('encr', args,
-            function(data) {
-                var $ta = document.getElementById('inputarea')
-                $ta.value = data},
-            "text")
+    var xhr = new XMLHttpRequest()
+    var ta = document.getElementById('inputarea')
+    var rd_mode = document.getElementsByName('rot_mode')
+    var args = new FormData();
+
+    /* get encryption mode */
+    for (var i = 0; i < rd_mode.length; i++) {
+        if (rd_mode[i].checked)
+            mode = rd_mode[i].value
+    }
+
+    /* request data */
+    args.append('rot_mode', mode)
+    args.append('text', ta.value)
+
+    /* send POST request */
+    xhr.open('POST', 'encr', true)
+    xhr.onload = function() {
+                var ta = document.getElementById('inputarea')
+                ta.value = this.responseText
+    }
+    xhr.send(args)
 }
 
 function loadFile()
 {
-    $('#in-lfile').trigger('click')
+    inf = document.getElementById('in-lfile')
+    inf.click()
 }
 
 function clearText()
 {
-    var $ta = document.getElementById('inputarea')
-    $('#in-lfile').val("")
-    $ta.value = ""
+    var ta = document.getElementById('inputarea')
+    var btn = document.getElementById('in-lfile')
+    ta.value = ""
+    btn.value = ""
 }
 
+/* provide textarea content as file download */
 function saveText()
 {
-    var $ta = document.getElementById('inputarea')
-    download("rot13.txt", $ta.value)
+    var ta = document.getElementById('inputarea')
+    download("rot13.txt", ta.value)
 }
 
 function download(filename, text)
